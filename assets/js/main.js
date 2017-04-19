a80c5b64cdc6b9e6f67731523bf16c247541217d
Emmett Lam <lam.emmett@gmail.com>
2017-04-19 00:07:57 -0700

    Added to projects section and imported Remodal plugin

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 	$(".wrapper, nav").css("opacity", 1);
 	
 	// Add smooth scrolling to all links
-	$("a").on('click', function(event) {
+	$(".navbar a").on('click', function(event) {
 		// Make sure this.hash has a value before overriding default behavior
 		if (this.hash !== "") {
 		  // Prevent default anchor click behavior
